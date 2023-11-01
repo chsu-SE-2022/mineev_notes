de843cbcc453ed8145633b1b4c1d6f9adf0a24ae
Ivan Mineev <146626565+1Cryo1@users.noreply.github.com>
2023-11-01 11:16:26 +0300

    vault backup: 2023-11-01 11:16:26

---
+++ Second year of study/Лабы по ООП/лаб 1/Lab1/Lab1/FileName.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+class printer {
+private:
+	char* model;
+	int trays, speed;
+	float quality;
+public:
+	void print() const;
+	void input(const char* x1, int x2, int x3, float x4);
+	void copy(printer &a);
+	bool compare(const &a);
+};
+
+void printer::input(const char* x1, int x2, int x3, float x4);
+{
+	model = new char[srtlen(x1) + 1];
+	strcpy(model, x1);
+	speed = x2;
+	lotki = x3;
+
+}
+
+void printer::print() const
+{
+	cout << "Model:" << model << endl;
+	cout << "Speed:" << speed << endl;
+	cout << "Trays:" << trays << endl;
+	cout << "Quality:" << quality << endl;
+	cout << endl;
+
+}
+
+
