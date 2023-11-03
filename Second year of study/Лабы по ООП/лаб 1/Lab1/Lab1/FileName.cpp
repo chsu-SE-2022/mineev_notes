5920891495d2f5071e2015084db2df9dc76f4245
Ivan Mineev <146626565+1Cryo1@users.noreply.github.com>
2023-11-03 09:33:59 +0300

    vault backup: 2023-11-03 09:33:59

--- Second year of study/Лабы по ООП/лаб 1/Lab1/Lab1/FileName.cpp
+++ Second year of study/Лабы по ООП/лаб 1/Lab1/Lab1/FileName.cpp
@@ -11,12 +11,12 @@ private:
 	float quality;
 public:
 	void print() const;
-	void input(const char* x1, int x2, int x3, float x4);
+	void input(char*x1, int x2, int x3, float x4);
 	void copy(printer &a);
 	bool compare(const &a);
 };
 
-void printer::input(const char* x1, int x2, int x3, float x4);
+void printer::input(char*x1, int x2, int x3, float x4);
 {
 	model = new char[srtlen(x1) + 1];
 	strcpy(model, x1);
